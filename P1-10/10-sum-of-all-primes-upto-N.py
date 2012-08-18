26713b804b64ac73ca4e815412eb5626825c5dbb
rrampage <raunak1001@gmail.com>
2012-08-18 06:42:43 +0530

    Added Sieve method to code. Blazing fast now. A 2000 year old lesson learnt

--- P1-10/10-sum-of-all-primes-upto-N.py
+++ P1-10/10-sum-of-all-primes-upto-N.py
@@ -22,9 +22,21 @@ def isPrime(x):
 	    return 0
     return 1
 
+def sieve(n):
+    numbers = range(0, n)
+    for prime in numbers:
+        if prime < 2:
+            continue
+        elif prime > n ** 0.5:
+            break
+        for i in range(prime ** 2, n, prime):
+            numbers[i] = 0
+    return [x for x in numbers if x > 1]
+
 
 u = int(sys.argv[1])
-print quickSum(u)
+#print quickSum(u)
+print sum(sieve(u))
 
 #----DEPRECIATED--------#
 
