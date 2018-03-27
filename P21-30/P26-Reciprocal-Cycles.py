ab26f75527a64df03ec50d49b6e32836fd371c4d
rrampage <raunak1001@gmail.com>
2018-03-27 19:17:59 +0530

    Added notes from forums for P26

--- P21-30/P26-Reciprocal-Cycles.py
+++ P21-30/P26-Reciprocal-Cycles.py
@@ -1,6 +1,22 @@
 import re
 from decimal import *
 
+"""
+Find the number d between 1 and 1000 s.t 1/d has longest recurring cycle
+
+My approach:
+        A cycle can be spotted using regex. 1/d is always of form 0.\d+
+        If it has a recurring cycle, 0.\d+?(\d+?)\1\d+
+        I need to calculate the division to at least as many digits as 2 * d (as my regex checks for at least 2 repitions using look ahead)
+        Python's decimal module gives arbitrary precision division
+
+Efficient approach:
+        After solving the problem, I read discussions in the forum about rep-units
+        Full reptends are primes such that 1/p has cycle of p-1 digits. Given my answer, it should have occurred to me earlier :P
+        7, 17, 19, 23 are all Full Reptend primes and can be found using Fermat's Little Theorem
+        Link : http://mathworld.wolfram.com/FullReptendPrime.html
+"""
+
 pat = re.compile(r'0.\d+?(\d+?)\1\1\1\d+')
 
 def largest_cycle(n, p):
